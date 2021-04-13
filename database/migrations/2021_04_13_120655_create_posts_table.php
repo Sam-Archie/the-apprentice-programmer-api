27c034ac984599cc3619752e8711d339e15506d6
Sam Archie <samuelarchie1@gmail.com>
2021-04-13 15:18:31 +0100

    Initial DB structure comeplete with models updated with apporpriate relationships

---
+++ database/migrations/2021_04_13_120655_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("title")->nullable();
+            $table->string("user_name")->nullable();
+            $table->text("content")->nullable();
+            $table->integer("likes")->nullable();
+            $table->string('image')->nullable();
+            $table->string('image_path')->nullable();
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
