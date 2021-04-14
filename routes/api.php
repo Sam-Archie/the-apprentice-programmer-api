29f3c8e491e5c604af61e3ec462e5b794b6f5fd1
Paul Dredge <dredgos@googlemail.com>
2021-04-14 11:47:03 +0100

    API routes created and tested

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +22,37 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/posts", [PostController::class, "index"]);
+
+Route::group(["prefix" => "/posts"], function() {
+    Route::get("", [PostController::class, "index"]);
+    Route::post("", [PostController::class, "store"]);
+
+    Route::group(["prefix" => "{post}"], function() {
+        Route::get("", [PostController::class, "show"]);
+        Route::put("", [PostController::class, "update"]);
+        Route::delete("", [PostController::class, "destroy"]);
+
+        Route::group(["prefix" => "tags"], function() {
+            Route::get("", [TagController::class, "show"]);
+            Route::put("", [TagController::class, "update"]);
+            Route::delete("", [TagController::class, "destroy"]);
+        });
+
+        Route::group(["prefix" => "comments"], function() {
+            Route::get("", [CommentController::class, "show"]);
+            Route::put("", [CommentController::class, "update"]);
+            Route::delete("", [CommentController::class, "destroy"]);
+        });
+    });
+});
+
+Route::group(["prefix" => "/users"], function() {
+    Route::get("", [UserController::class, "index"]);
+    Route::post("", [UserController::class, "store"]);
+
+    Route::group(["prefix" => "{user}"], function() {
+        Route::get("", [UserController::class, "show"]);
+        Route::put("", [UserController::class, "update"]);
+        Route::delete("", [UserController::class, "destroy"]);
+    });
+});
